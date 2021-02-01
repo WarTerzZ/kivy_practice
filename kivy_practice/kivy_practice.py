900cf47d607169dafd6126c4e92699d517cc668d
WarTerzZ <mmanzini@ska.ac.za>
2021-02-01 08:37:26 +0200

    Added label display code Added paintWidget display code

--- kivy_practice/kivy_practice.py
+++ kivy_practice/kivy_practice.py
@@ -1,13 +1,29 @@
-from kivy.app import App
-from kivy.uix.label import Label
+#from kivy.app import App
+#from kivy.uix.label import Label
 
-class MainApp(App):
-    def build(self):
-        label = Label(text="Hello from Kivy")
-        # size_hint(0.5, 0.5), pos_hint("center_x": 0.5, "center_y": 0.5)
-        # )
-        return label
+#class MainApp(App):
+#    def build(self):
+#        label = Label(text="Hello from Kivy")
+#        return label
 
-if __name__ == "__main__":
-    app = MainApp()
-    app.run()
+#if __name__ == "__main__":
+#    app = MainApp()
+#    app.run()
+
+#from kivy.app import App
+#from kivy.uix.widget import Widget
+#from kivy.graphics import Color, Ellipse
+
+#class MyPaintWidget(Widget):
+#    def on_touch_down(self, touch):
+#        Color(1,1,0)
+#        d = 500.
+#        Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d,d))
+
+#class MyPaintApp(App):
+#    def build(self):
+#        return MyPaintWidget()
+
+#if __name__ == "__main__":
+#    MyPaintApp().run()
+
